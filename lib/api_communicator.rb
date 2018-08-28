ed7c44cdc129ef52f9c588cea3ccae8b8fc09c05
github username <github email address>
2018-08-28 15:48:26 -0400

    all changes

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,12 +2,27 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
 def get_character_movies_from_api(character)
   #make the web request
+  new_array = []
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+    response_hash["results"].each do |character_data|
+      if character_data["name"].downcase == character.downcase
+      character_data["films"].each do |film|
+        #make web request to each of those URLS using Rest Client
+        client_request = RestClient.get(film)
+        #collect and parse the responses of each of these web requests
+        #array of hashes: each of which represents a movie
+        new_array << JSON.parse(client_request)
+      end
+    end
+  end
+new_array
+end
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
 
@@ -20,15 +35,21 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
-end
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |x|
+    puts "Title: #{x["title"]}"
+    puts "Director: #{x["director"]}"
+    puts "Producer: #{x["producer"]}"
+    puts "*************************\n\n\n"
+  end
 end
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
+
 end
 
 ## BONUS
